64991c081d2b5ebea6d9283cf97dcbba4d293e4a
Abdallah <abdallahantony55.aa@gmail.com>
2021-11-30 05:30:46 -0800

    logo added

---
+++ storage/framework/views/a537389f778ce610382d1460e64299c4ea2896a5.php
@@ -0,0 +1,96 @@
+
+
+<?php $__env->startSection('content'); ?>
+     <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Logo Edit</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Logo Edit</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card card-primary">
+            <div class="card-header">
+              <h3 class="card-title">Edit Logo</h3>
+
+              <div class="card-tools">
+                <a href="#" class="btn btn-tool">
+                  <i class="fas fa-list"></i></a>
+              </div>
+            </div>
+            <div class="card-body">
+        <form method="POST" action="<?php echo e(route('logos.update')); ?>" enctype="multipart/form-data">
+            <?php echo csrf_field(); ?>
+            <?php echo method_field('PUT'); ?>
+            <div class="form-group col-lg-6">
+              <label for="inputName">Logo Text</label>
+              <input type="text" id="inputName" name="text" value="<?php echo e($logo->text??''); ?>"" class="form-control <?php $__errorArgs = ['text'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> form-control is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>"">
+              <?php $__errorArgs = ['text'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+              <small class="text-danger">*<?php echo e($errors->first('text')); ?>*</small> 
+              <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+              </div>       
+
+              <div class="form-group col-6">
+                <label for="exampleFormControlFile1">File input</label>
+                <input name="image" type="file" class="form-control-file" id="exampleFormControlFile1">
+                <?php $__errorArgs = ['image'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                <small class="text-danger">*<?php echo e($errors->first('image')); ?>*</small> 
+                <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+                <?php if($logo): ?>
+                   <img src="<?php echo e(asset($logo->image->url)); ?>" alt="..." class="img-thumbnail">
+                <?php endif; ?>
+              </div>
+              
+              <div class="form-group text-center">
+                <input type="submit" value="Update" class="btn btn-success ">
+               </div>
+            </div>
+        </form>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.apps', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\projects\web\laravel\progress\my_web1\resources\views/logos/edit.blade.php ENDPATH**/ ?>
--- storage/framework/views/dc2c6f1a9887cd6f7cbdf7a0ee8bf4d4eaf822d9.php
+++ storage/framework/views/dc2c6f1a9887cd6f7cbdf7a0ee8bf4d4eaf822d9.php
@@ -44,7 +44,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container-fluid">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">
-			<img class="img-fluid" src="images/logo.png')}}" alt="" />
+			<img class="img-fluid" src="<?php echo e(asset($logo->image->url)); ?>" alt="" />
 		</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
