0006c5c297661ffae144f6a0a104e8c103363e60
Abdallah <abdallahantony55.aa@gmail.com>
2021-12-01 03:57:49 -0800

    added message factony

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
        // \App\Models\Crud::factory(101)->create();
-        //\App\Models\Message::factory(101)->create();
+        \App\Models\Message::factory(1001)->create();
         \App\Models\FoodMenu::factory(10)->create();
     }
 }
